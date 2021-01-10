59e776086333dde41793f20e20227b2edd4082f8
Josey Kinnaman <josey.kinnaman@planetargon.com>
2021-01-09 16:43:18 -0800

    Add LazyLoad outline and export

---
+++ src/components/LazyLoad.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import './LazyLoad.css';
+
+const LazyLoad = () => {
+
+  return(
+    <div className="">
+
+    </div>
+  )
+}
+
+export default LazyLoad;
