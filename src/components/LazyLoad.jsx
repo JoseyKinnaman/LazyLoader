74b16dc9323e2525ec0b8736833a749ec86505c5
Josey Kinnaman <josey.kinnaman@planetargon.com>
2021-01-09 16:49:21 -0800

    Add useEffect hook and img tag

--- src/components/LazyLoad.jsx
+++ src/components/LazyLoad.jsx
@@ -4,9 +4,18 @@ import './LazyLoad.css';
 const LazyLoad = ({src, placeholder, alt}) => {
   const [loading, setLoading] = useState(true);
   const [img, setImg] = useState(placeholder);
+
+  useEffect(() => {
+    const lazyImage = new Image();
+    lazyImage.src = src;
+    lazyImage.onload = () => {
+      setLoading(false);
+      setImg(src);
+    }
+  }, [src])
   return(
     <div className="">
-
+      <img src={src} alt={alt}/>
     </div>
   )
 }
